bcc2265a6859454be00c4bc379c3e46abe10e0a7
apphow <apehow111@gmail.com>
2020-07-04 11:30:51 -0400

    got rid of redundant code. Added changeDiceFaces function so leftDiceNumber and rightDiceNumber calls it

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void changeDiceFaces() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,11 +42,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                  print('diceNumber = $leftDiceNumber');
-                });
+                changeDiceFaces();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -46,11 +50,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
                 onPressed: () {
-                  setState(() {
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    print('rightDice = $rightDiceNumber');
-                  });
+                  changeDiceFaces();
                 },
                 child: Image.asset('images/dice$rightDiceNumber.png')),
           ),
